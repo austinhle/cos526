75b6f0327ea081c3a39224dc45d6437d43e8942f
Austin Le <austin.h.le@gmail.com>
2018-11-12 12:14:03 -0500

    Rename main program to icp

---
+++ a2/icp.cpp
@@ -0,0 +1,28 @@
+#include "pointcloud.h"
+#include "matrix4x4.h"
+#include "vector4D.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+  // PointCloud pc;
+  // pc.loadPointCloud("bun000.pts");
+  // const vector<Point>& points = pc.getPoints();
+  // Point p = points[0];
+  // cout << p.toString() << endl;
+
+  // Matrix4x4 m;
+  // m.loadMatrix("bun045.xf");
+  //
+  // Matrix4x4 inv = m.inverse();
+  //
+  // m.printMatrix();
+  // inv.printMatrix();
+  //
+  // Matrix4x4 id = m * inv;
+  // id.printMatrix();
+
+  return 0;
+}
