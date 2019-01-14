6559eedf2a4d89609710847852b79158a7777ad4
Austin Le <austin.h.le@gmail.com>
2019-01-13 20:37:39 -0500

    Add indirect illum estimate from photon maps; add clamping of colors

--- a3/src/render.h
+++ a3/src/render.h
@@ -2,4 +2,5 @@
 #include "photon.h"
 
 R2Image *RenderImage(R3Scene *scene, PhotonMap *global_photon_map,
-  PhotonMap *caustic_photon_map, int width, int height, int print_verbose);
+  PhotonMap *caustic_photon_map, int num_nearest_photons,
+  int width, int height, int print_verbose);
